8151a6498ff8f44537e9f06dcd613b383e0170cd
jay <jayyehice@gmail.com>
2022-09-27 17:38:25 +0800

    adjust js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,43 @@
 
 
 const App = {
-    data() {
-      return {
-        level:7,
-        checkClass: ["", "", "", "", ""],
-        examList: [[1, 2], [3, 4], [5, 6], [7, 8], [9, 9]],
-        ans:["", "", "", "", ""],
-      }
-    },
-    
-    methods:{
-      changeIndex(idx){
-        const rightAns = this.examList[idx][0]*this.examList[idx][1];
-        
-        if(rightAns == this.ans[idx]){
-          this.checkClass[idx] = "right";
-        }else{
-          this.checkClass[idx] = "err";
-          console.log(rightAns);
-        }
-      }
-    },
-    mounted() {
-      for(let i=0; i<this.examList.length; i++){
-        this.examList[i][0] = Math.floor(Math.random()*this.level)+2;
-        this.examList[i][1] = Math.floor(Math.random()*this.level)+2;
+  data() {
+    return {
+      level: 7,
+      checkClass: ["", "", "", "", ""],
+      examList: [[1, 2], [3, 4], [5, 6], [7, 8], [9, 9]],
+      ans: ["", "", "", "", ""],
+    }
+  },
+
+  methods: {
+    changeIndex(idx) {
+      const rightAns = this.examList[idx][0] * this.examList[idx][1];
+
+      if (rightAns == this.ans[idx]) {
+        this.checkClass[idx] = "right";
+      } else {
+        this.checkClass[idx] = "err";
+        console.log(rightAns);
       }
     }
-    
+  },
+  mounted() {
+    const firstNum = Math.floor(Math.random() * this.level) + 2;
+
+    let arr = [2, 3, 4, 5, 6, 7, 8, 9];
+    arr.sort(function () {
+      return (0.5 - Math.random());
+    });
+
+    for (let i = 0; i < this.examList.length; i++) {
+      this.examList[i][0] = firstNum;
+      this.examList[i][1] = arr[i];
+    }
   }
-  
-  Vue.createApp(App).mount('#app')
+
+}
+
+Vue.createApp(App).mount('#app')
 
 
